642cd110e8e4859db918609a9abbfa5b27718576
anjali sharma <anjisharma649@gmail.com>
2024-10-09 08:04:17 +0545

    commit

--- resources/views/registeredUser/Ad/postAd.blade copy.php
+++ resources/views/registeredUser/Ad/postAd.blade copy.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <div class="block lg:grid grid-cols-1 lg:grid-cols-3 gap-3 mx-4 lg:mx-16 mb-20">
+    <div class=" font-manrope block lg:grid grid-cols-1 lg:grid-cols-3 gap-3 mx-4 lg:mx-16 mb-40">
         <!-- Left Content: Categories and Tabs -->
         <div class="col-span-2 mt-8 lg:mt-14">
             <h1 class="font-medium text-lg lg:text-xl">Choose Category below to post your ad</h1>
@@ -31,6 +31,7 @@
                     </div>
                     <!-- Health Tab -->
                     <div>
+
                         @if (is_array($registeredUser->category) &&
                                 in_array(healthCareCategories()->first()?->mainCategory?->title_en, $registeredUser->category))
                             <button type="button" class="text-green-900 text-base lg:text-xl font-semibold"
@@ -114,22 +115,31 @@
                 <div class="mt-3">
                     <div id="unstyled-tabs-1" role="tabpanel" aria-labelledby="unstyled-tabs-item-1">
                         <ul>
-                            @foreach (propertyCategories() as $propertyCategory)
-                                <a href="{{ route('registeredUser.propertyCategory.create', $propertyCategory) }}">
-                                    <li class="text-blue-900 text-base lg:text-xl font-semibold">
-                                        {{ $propertyCategory->title_en }}</li>
-                                </a>
-                            @endforeach
+                            @if (is_array($registeredUser->category) &&
+                                    in_array(propertyCategories()->first()?->mainCategory?->title_en, $registeredUser->category))
+
+                                @foreach (propertyCategories()->first() as $propertyCategory)
+                                    <a href="{{ route('registeredUser.propertyCategory.create', $propertyCategory) }}">
+                                        <li class="text-blue-900 text-base lg:text-xl font-semibold">
+                                            {{ $propertyCategory->title_en }}</li>
+                                    </a>
+                                @endforeach
+                            @endif
                         </ul>
                     </div>
                     <div id="unstyled-tabs-2" class="hidden" role="tabpanel" aria-labelledby="unstyled-tabs-item-2">
                         <ul>
-                            @foreach (healthCareCategories() as $healthCareCategory)
-                                <a href="{{ route('registeredUser.healthCareCategory.create', $healthCareCategory) }}">
-                                    <li class="text-green-900 text-base lg:text-xl font-semibold">
-                                        {{ $healthCareCategory->title_en }}</li>
-                                </a>
-                            @endforeach
+                            @if (is_array($registeredUser->category) &&
+                                    in_array(healthCareCategories()->first()?->mainCategory?->title_en, $registeredUser->category))
+
+                                @foreach (healthCareCategories()->first() as $healthCareCategory)
+                                    <a
+                                        href="{{ route('registeredUser.healthCareCategory.create', $healthCareCategory) }}">
+                                        <li class="text-green-900 text-base lg:text-xl font-semibold">
+                                            {{ $healthCareCategory->title_en ??''}}</li>
+                                    </a>
+                                @endforeach
+                            @endif
                         </ul>
                     </div>
                     <div id="unstyled-tabs-3" class="hidden" role="tabpanel" aria-labelledby="unstyled-tabs-item-3">
