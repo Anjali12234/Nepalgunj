e25eeee6bca68ce10ab17064af72107dbc4d98b3
anjali12234 <anjisharma649@gmail.com>
2025-06-12 22:43:01 +0545

    news controller changed

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -55,7 +55,7 @@ class NewsController extends Controller
 
         alert("form updated");
 
-        return redirect(route('admin.news.index'));
+        return redirect(route('admin.newsList.index'));
     }
 
 
